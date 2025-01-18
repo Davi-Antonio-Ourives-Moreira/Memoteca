705991e2979014b74ab90a060bfe2822aa317761
Davi-Antonio-Ourives-Moreira <daviaomoreira@gmail.com>
2025-01-18 10:57:15 -0300

    pagina memoteca

---
+++ src/app/app.routes.ts
@@ -0,0 +1,33 @@
+import { Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentosComponent } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos.component';
+import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'ListasPensamentos',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'CriarPensamento',
+    component: CriarPensamentoComponent
+  },
+
+  {
+    path: 'ListasPensamentos',
+    component: ListarPensamentosComponent
+  },
+
+  {
+    path: 'pensamentos/excluirPensamento/:id',
+    component: ExcluirPensamentoComponent
+  },
+
+  {
+    path: 'pensamentos/editarPensamento/:id',
+    component: EditarPensamentoComponent
+  }
+];
