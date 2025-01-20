32945b892c5fa0956aaf3c7e606c3f781852bf41
Davi-Antonio-Ourives-Moreira <daviaomoreira@gmail.com>
2025-01-20 08:56:31 -0300

    acrescentando botao carregar mais

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -4,23 +4,43 @@ import { PensamentoComponent } from '../pensamento/pensamento.component';
 import { CommonModule } from '@angular/common';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
+import { BotaoCarregarMaisComponent } from "./botao-carregar-mais/botao-carregar-mais.component";
 
 @Component({
   selector: 'app-listar-pensamentos',
   standalone: true,
-  imports: [RouterModule, PensamentoComponent, CommonModule],
+  imports: [RouterModule, PensamentoComponent, CommonModule, BotaoCarregarMaisComponent],
   templateUrl: './listar-pensamentos.component.html',
   styleUrl: './listar-pensamentos.component.css'
 })
 export class ListarPensamentosComponent implements OnInit {
   listaPensamentos:Pensamento[] = []
 
+  paginaAtual = 1
+
+  haMaisPensamentos:boolean = true;
+
   constructor (private service: PensamentoService) {}
 
   ngOnInit(): void {
-    this.service.listar().subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos
+
+      if (this.listaPensamentos.length < 6){
+        this.haMaisPensamentos = false
+      }
     });
+  }
+
+  carregarMais(){
+    this.service.listar(++this.paginaAtual).subscribe(listaPensamentos => {
+      this.listaPensamentos.push(...listaPensamentos)
+
+      if (!this.listaPensamentos.length){
+        this.haMaisPensamentos = false;
+      }
+
+    })
   }
 
 }
