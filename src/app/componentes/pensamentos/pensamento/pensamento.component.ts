060d9d72ffe4d4512a84e626b532debaa8d1ab2e
Davi-Antonio-Ourives-Moreira <daviaomoreira@gmail.com>
2025-01-20 23:02:00 -0300

    mudança card autoria

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -31,7 +31,7 @@ export class PensamentoComponent implements OnInit {
   }
 
   larguraCardPensamento(): string {
-    if (this.pensamento.conteudo.length >= 256){
+    if (this.pensamento.conteudo.length >= 256 || this.pensamento.autoria.length >= 256){
       return 'pensamento-g';
     }
 
